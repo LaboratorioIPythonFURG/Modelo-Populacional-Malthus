05e9b506def57364704051ca27d4da6b37ea7e6f
Jhonatan-Biller <jhonatanrodriguesbiller@gmail.com>
2022-04-19 16:26:01 -0300

    atualizacao

--- python/diferentes_passos.py
+++ python/diferentes_passos.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sol_euler import sol_euler
 from sol_euler_mod import sol_euler_mod
+from sol_rk4 import sol_rk4
 import matplotlib.pyplot as plt
 
 def diferentes_passos(ti,tf,passos,P0,cores,modelo,metodo):
@@ -19,7 +20,18 @@ def diferentes_passos(ti,tf,passos,P0,cores,modelo,metodo):
             elif metodo == 'sol_euler_mod':
                 
                 P = sol_euler_mod(vt,P0,'malthus')
+            
+            elif metodo == 'sol_rk4':
+                
+                P = sol_rk4(vt,P0,'malthus')
         
+        elif modelo == 'Verhulst':
+            
+            if metodo == 'sol_euler':
+                
+                P = sol_euler(vt,P0,'verhulst')
+            
+                
         #Grafico da curva    
         plt.plot(vt,P,'{}'.format(cores[np.where(passos==passo)[0][0]]),label='h = {}'.format(passo))
 
--- python/diferentes_passos_erro.py
+++ python/diferentes_passos_erro.py
@@ -5,14 +5,14 @@ from f_erro import f_erro
 
 def diferentes_passos_erro(ti,tf,passos,P0,cores,modelo,metodo):
 
+    err = 0
+    P = 0
+    
     #Criando loop para diferentes passos de derivação
     for passo in passos:
 
         #Criando vetor tempo 
         vt = np.arange(ti,tf,passo)
-
-        #Chamando a função sol_euler 
-        P = sol_euler(vt,P0,'malthus')
         
         if modelo == 'Malthus':                        
                 
@@ -21,9 +21,29 @@ def diferentes_passos_erro(ti,tf,passos,P0,cores,modelo,metodo):
 
             if metodo == 'sol_euler':
             
+                #Chamando a função sol_euler 
+                P = sol_euler(vt,P0,'malthus')
+            
                 #Chamando a função erro
                 err = f_erro(sol_analitica,P)
-
+                
+        elif modelo == 'Verhulst':
+            
+            #Gerando solução analítica
+            sol_analitica = 186488*np.exp(0.0056*vt)
+            
+            if metodo == 'sol_euler':
+            
+                #Chamando a função sol_euler 
+                P = sol_euler(vt,P0,'verhulst')
+            
+                #Chamando a função erro
+                err = f_erro(sol_analitica,P)
+                
+        else:
+            
+            print('Modelo não suportado')
+            
         #Grafico da curva    
         plt.plot(vt,err,'{}'.format(cores[np.where(passos==passo)[0][0]]),label='h = {}'.format(passo))
 
