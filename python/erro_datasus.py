1ce40fd9f5c6bc7b08293ad3820a9992682a840b
Jhonatan-Biller <jhonatanrodriguesbiller@gmail.com>
2022-06-18 22:47:35 -0300

    att

---
+++ python/erro_datasus.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+
+def erro_datasus(dados_datasus,solucoes,metodo):
+    
+    dados = [[0, 0, 0]]
+    
+    for i in range(0,3):
+        
+        if metodo=='lsoda':
+            
+            solucao = solucoes[i][0]
+            
+        elif metodo=='rk45':
+            
+            solucao = solucoes.y[0][i]
+        
+        else:
+            
+            solucao = solucoes[i]
+            
+        erro_hab = dados_datasus[i] - solucao
+        #print(erro_hab)
+        erro_perc = (erro_hab/dados_datasus[i])*100
+        #Adicionando linhas à matriz (tabela)
+        dados = np.vstack([dados,[solucao,round(erro_hab),str(round(erro_perc,2))+'%']])
+        
+    #remove a primeira linha da matriz
+    dados = np.delete(dados, 0, 0)
+    
+    return dados
