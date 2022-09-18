75bd5eb60918b87f8d86ef38f81736ff6311a1f3
Jhonatan-Biller <jhonatanrodriguesbiller@gmail.com>
2022-09-18 11:53:35 -0300

    Montroll Analítico

--- python/erro_ibge.py
+++ python/erro_ibge.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 def erro_ibge(index,solucao_IBGE,solucoes,metodos):
@@ -8,10 +7,10 @@ def erro_ibge(index,solucao_IBGE,solucoes,metodos):
     for i in index:
         erro_hab = solucao_IBGE - solucoes[i]
         erro_perc = (erro_hab/solucao_IBGE)*100
-        #Adicionando linhas à matriz (tabela)
         dados = np.vstack([dados,[metodos[i],round(erro_hab),str(round(erro_perc,2))+'%']])
         
-    #remove a primeira linha da matriz
     dados = np.delete(dados, 0, 0)
     
     return dados
+
+
