a50cd747a07cf2e9481be87f7ea18cf2f895cdd6
Jhonatan Rodrigues Biller <53588180+Jhonatan-Biller@users.noreply.github.com>
2024-01-07 09:57:46 -0300

    Update sol_euler.py

--- python/sol_euler.py
+++ python/sol_euler.py
@@ -1,16 +1,12 @@
-
 import numpy as np #Biblioteca
 
-def sol_euler(vt,P0,modelo): #Função Euler
+def sol_euler(vt,P0,modelo): #Função Euler       
         
     P = np.array([P0]) #Vetor população
-    
     h = vt[2] - vt[1] #Passo de derivação
-
+        
     for t in vt: #Aproximando P(i) pelo método de Euler
-        
         Pi = P[-1]+h*modelo(P[-1])        
-        
         P = np.append(P,Pi)
-        
+            
     return P[0:P.shape[0]-1]
